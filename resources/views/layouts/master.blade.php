02c1611b2d20c9437448c16e0be0d4a203a23daa
Rama Febriyan <dragoon.redeyed@gmail.com>
2020-08-10 22:11:32 +0700

    berhasil memasangkan script alert

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -99,10 +99,10 @@
   <script src="{{asset('/sbadmin2/js/sb-admin-2.min.js')}}"></script>
 
   <!-- script tambahan sweet alert, bukan dari bawaan sb-admin-2 -->
-  @stack('scripts')
+  
 
   <script src="{{asset('/sbadmin2/js/swal.min.js')}}"></script>
-
+  @stack('scripts')
 </body>
 
 </html>
